22f1c66a8befc9a1fc6c2918b83e27ebaec225e3
xy285 <hari20020511@gmail.com>
2024-04-27 14:03:50 -0400

    1

--- JavaLeetCode/JavaLeetCode/src/Leetcode/Leetcode1657IfTwoStringAreEqual/Solution.java
+++ JavaLeetCode/JavaLeetCode/src/Leetcode/Leetcode1657IfTwoStringAreEqual/Solution.java
@@ -1,6 +1,12 @@
 package Leetcode.Leetcode1657IfTwoStringAreEqual;
 
 public class Solution {
+    public boolean closeStrings(String word1, String word2) {
+        //If The Frequence happening is the same
+
+        //If
+        return false;
+    }
     public static void main(String[] args) {
         System.out.println(1);
     }
